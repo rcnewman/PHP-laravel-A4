31998aa53fb14783229069222dded6065a7a6fc2
Ross Newman <rosscnewman@gmail.com>
2014-02-17 12:35:06 -0800

    changed dvd_titles table to dvds

--- app/models/Dvd.php
+++ app/models/Dvd.php
@@ -2,13 +2,13 @@
 class Dvd{
 	public static function search($dvd_title,$genre,$rating)
 	{
-		$query = DB::table('dvd_titles')
+		$query = DB::table('dvds')
 		->select('title' ,DB::raw("DATE_FORMAT(release_date,'%b %d %Y') AS release_date"),'format_name','genre_name','label_name','rating_name','sound_name')  // dont use DB:raw on user input
-		->join('formats','formats.id','=','dvd_titles.format_id')
-		->join('genres','genres.id','=','dvd_titles.genre_id')
-		->join('labels','labels.id','=','dvd_titles.label_id')
-		->join('ratings','ratings.id','=','dvd_titles.rating_id')
-		->join('sounds','sounds.id','=','dvd_titles.sound_id');
+		->join('formats','formats.id','=','dvds.format_id')
+		->join('genres','genres.id','=','dvds.genre_id')
+		->join('labels','labels.id','=','dvds.label_id')
+		->join('ratings','ratings.id','=','dvds.rating_id')
+		->join('sounds','sounds.id','=','dvds.sound_id');
 
 		if($dvd_title){
 			$query->where('title','LIKE',"%$song_title%");
